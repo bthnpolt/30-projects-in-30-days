6b3ced2f721fca0679ceedb621f72c1abc0543a5
bthnpolt <139389577+bthnpolt@users.noreply.github.com>
2024-10-01 14:40:34 +0300

    Day-3

---
+++ Day-3(light & dark theme)/app.js
@@ -0,0 +1,9 @@
+const html =  document.querySelector('html');
+const themeBtn = document.querySelector('button');
+
+
+const themeChange = (e) =>{
+    html.classList.toggle('dark-theme');
+}
+themeBtn.addEventListener('click', themeChange);
+
